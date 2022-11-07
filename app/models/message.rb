60df8f7fd45b9895bbaf857bab4b7424b3081a42
Max <maximzakharov5@gmail.com>
2022-11-07 21:16:48 +0300

    Complete input-box and create messages custom scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: {minimum: 3, maximum: 200}
+  scope :custom_display, -> { order(:created_at).last(20) }
 end
